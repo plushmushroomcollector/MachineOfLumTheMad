0d5da83f59323cd6e334df958e713ce51ab2a00f
Athemia Agraxis <alice.m.l.archer@gmail.com>
2024-05-09 02:09:05 -0700

    initial commit

---
+++ discord_bot/discord_bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord import Intents
+from discord.ext.commands import Bot
+import discord.ext.commands as commands
+
+class ConfigError(Exception): pass
+class PluginDirectoryError(Exception): pass
+class PluginError(Exception): pass
+
+class My_Bot(Bot):
+    INTENTS = Intents.default()
+    INTENTS.members = True
+    INTENTS.message_content = True
+    INTENTS.reactions = True
+    INTENTS.dm_messages = True
+    INTENTS.dm_reactions = True
+    key = ""
+
+    def __init__(self, key, config_file, *args, **kwargs):
+        super().__init__(
+            *args,
+            case_insensitive=True,
+            intents = My_Bot.INTENTS,
+            **kwargs
+        )
+        self.key = key
+        self.config = config_file
+
+    def run_bot(self, *args, **kwargs):
+        super().run(self.key, *args, **kwargs)
---
+++ run.py
@@ -0,0 +1,71 @@
+import discord
+from discord import Intents
+from config import CONFIG
+from plugins.administrative_tools import administrative_tools
+from plugins.manifest import MANIFEST
+from plugins.manifest import IMPORT_LIST
+import datetime
+import importlib
+
+import functools as ft
+
+def is_admin(func):
+        @ft.wraps(func)
+        async def decorated(ctx, *args, **kwargs):
+            if ctx.author.id in CONFIG['ADMIN_LIST']:
+                return await func(ctx, *args, **kwargs)
+            else:
+                await ctx.send(f'{ctx.author.mention} you are not an admin')
+
+        return decorated
+
+def run_bot():
+    from discord_bot import My_Bot
+
+    my_bot = My_Bot(CONFIG['DISCORDAPPKEY'], CONFIG, command_prefix=CONFIG['COMMAND_PREFIX'])
+    
+    @my_bot.command(name='plugin-load', aliases=[])
+    @is_admin
+    async def plugin_load(ctx, *plugin_name : str):
+        plugin_name = ' '.join(plugin_name)
+        plugin_module = importlib.import_module(f'plugins.{MANIFEST[plugin_name]}')
+        plugin_class = getattr(plugin_module, MANIFEST[plugin_name])
+        await my_bot.add_cog(plugin_class(my_bot))
+        await ctx.send(f'plugin: {plugin_name} was loaded')
+
+    @my_bot.command(name='plugin-remove', aliases=['plugin-unload'])
+    @is_admin
+    async def plugin_remove(ctx, *plugin_name : str):
+        plugin_name = ' '.join(plugin_name)
+        await my_bot.remove_cog(plugin_name)
+        await ctx.send(f'plugin: {plugin_name} was unloaded')
+
+    @my_bot.command(name='plugin-reload', aliases=[])
+    @is_admin
+    async def plugin_reload(ctx, *plugin_name : str):
+        plugin_name = ' '.join(plugin_name)
+        await my_bot.remove_cog(plugin_name)
+        await ctx.send(f'plugin: {plugin_name} was unloaded')
+        plugin_module = importlib.import_module(f'plugins.{MANIFEST[plugin_name]}')
+        plugin_class = getattr(plugin_module, MANIFEST[plugin_name])
+        await my_bot.add_cog(plugin_class(my_bot))
+        await ctx.send(f'plugin: {plugin_name} was reloaded')
+
+    @my_bot.command(name='plugin-help', aliases=[])
+    async def plugin_help(ctx, *plugin_name : str):
+        plugin_name = ' '.join(plugin_name)
+        await ctx.send(my_bot.cogs.get(plugin_name).help)
+
+    @my_bot.event
+    async def on_ready():
+        print(f'We have logged in as {my_bot.user}')
+
+        for plugin_name in IMPORT_LIST:
+            plugin_module = importlib.import_module(f'plugins.{MANIFEST[plugin_name]}')
+            plugin_class = getattr(plugin_module, MANIFEST[plugin_name])
+            await my_bot.add_cog(plugin_class(my_bot))
+            print (f'module: {plugin_name} loaded')
+
+    my_bot.run_bot()
+
+run_bot()
